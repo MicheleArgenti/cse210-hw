0e327fcebd24b388b31b6d6d6d8f19dd003215be
raghork <michele.argenti000@gmail.com>
2025-05-09 13:02:24 +0200

    Added Exercise 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        String first_name = Console.ReadLine();
+        String firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        String last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        String stringGrade = Console.ReadLine();
+        int intGrade = int.Parse(stringGrade);
+        String letter = "";
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80 && intGrade < 90)
+        {
+            letter = "B";
+        }
+        else if (intGrade >= 70 && intGrade < 80)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60 && intGrade < 70)
+        {
+            letter = "D";
+        }
+        else if (intGrade < 60)
+        {
+            letter = "F";
+        }
+        if (stringGrade.Length > 1)
+        {
+            if (int.Parse(stringGrade.Substring(1)) >= 7 && letter != "A" && letter != "F")
+            {
+                letter += "+";
+            }
+            else if (int.Parse(stringGrade.Substring(1)) <= 3 && letter != "F")
+            {
+                letter += "-";
+            }
+        }
+        Console.WriteLine($"{letter}");
+        if (intGrade >= 70)
+        {
+            Console.Write("You passed!");
+        }
+        else
+        {
+            Console.Write("Better luck next time!");
+        }
     }
 }
