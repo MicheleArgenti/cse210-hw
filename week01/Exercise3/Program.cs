2ac6b6a5fa23848c5c59fd19c9b0c608170506d9
raghork <michele.argenti000@gmail.com>
2025-05-09 16:56:11 +0200

    Added Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Boolean guessed = false;
+        int numberOfGuess = 0;
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        while (!guessed)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            numberOfGuess += 1;
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                guessed = true;
+                Console.WriteLine($"You took {numberOfGuess} times to guess");
+                Console.WriteLine("Do you want to play again? (yes/no) ");
+                string playAgain = Console.ReadLine();
+                if (playAgain.ToLower() == "yes")
+                {
+                    guessed = false;
+                    numberOfGuess = 0;
+                    magicNumber = random.Next(1, 101);
+                }
+            }
+        }
     }
 }
